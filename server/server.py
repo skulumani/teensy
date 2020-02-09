db5968a4514f9ff1563da65c8bcbda61e30d3962
Shankar Kulumani <shanks.k@gmail.com>
2020-02-09 13:32:59 -0500

    Python serial reader

--- server/server.py
+++ server/server.py
@@ -1,5 +1,9 @@
 import serial
 
-ser = serial.Serial('/dev/cu.usbmodem5105671')
+ser = serial.Serial('/dev/cu.usbmodem5105671', 115200)
 print(ser.name)
+
+while True:
+    print(ser.readline())
+
 
