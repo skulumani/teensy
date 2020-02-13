0bb78498942766fbd4deb039cd66aa363f7c0d28
Shankar Kulumani <shanks.k@gmail.com>
2020-02-12 22:19:47 -0500

    Update with attempt at streams

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ static bool pb_print_write(pb_ostream_t *stream, const pb_byte_t *buf, size_t co
 }
 
 pb_ostream_s as_pb_ostream(Print& p) {
-    return {pb_print_write, &p, SIZE_MAX, 0};
+    return {&pb_print_write, &p, SIZE_MAX, 0};
 }
 
 static bool pb_stream_read(pb_istream_t *stream, pb_byte_t *buf, size_t count) {
@@ -33,33 +33,37 @@ static bool pb_stream_read(pb_istream_t *stream, pb_byte_t *buf, size_t count) {
 
 pb_istream_s as_pb_istream(Stream& s) {
     #ifndef PB_NO_ERRMSG
-        return {pb_stream_read, &s, SIZE_MAX, 0};
+        return {&pb_stream_read, &s, SIZE_MAX, 0};
     #else
-        return {pb_stream_read, &s, SIZE_MAX};
+        return {&pb_stream_read, &s, SIZE_MAX};
     #endif
 }
 
 /* extern "C" int main(void) */
 int main(void) {
-
+    
     AHRS::IMU imu;
+    usb_serial_class serial_usb;
+    serial_usb.begin(115200);
 
     AHRS_IMUMeasurement imu_msg = AHRS_IMUMeasurement_init_zero;
-    imu_msg.accel_meas[0] = 1.0;
-    imu_msg.accel_meas[1] = 2.0;
-    imu_msg.accel_meas[2] = 3.0;
 
-    pb_ostream_s pb_out = as_pb_ostream(Serial);
-    Serial.println("encoding");
-    bool status = pb_encode(&pb_out, AHRS_IMUMeasurement_fields, &imu_msg);
+    pb_ostream_s pb_out = as_pb_ostream(serial_usb);
+    serial_usb.println("starting");
     
     // pb_istream_s pb_in = as_pb_istream(Serial);
     // pb_decode(&pb_in, AHRS_IMUMeasurement_fields, &imu_msg)
     //
     // loop forever
     while (1) {
+        imu.imu.readSensor();
+        imu_msg.accel_meas[0] = imu.imu.getAccelX_mss();
+        imu_msg.accel_meas[1] = imu.imu.getAccelY_mss();
+        imu_msg.accel_meas[2] = imu.imu.getAccelZ_mss();
+
+        bool status = pb_encode(&pb_out, AHRS_IMUMeasurement_fields, &imu_msg);
+        serial_usb.println(pb_out.bytes_written);
         delay(1000);
-        Serial.println("test");
     }
 }
 
